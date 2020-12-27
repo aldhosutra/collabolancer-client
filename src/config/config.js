f1897e0164820e48eafef4a257788926bacb8705
Aldo Suhartono Putra <aldhosutra@gmail.com>
2020-12-28 01:21:36 +0700

    add environment variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  nodes: ["http://localhost:4000"],
-  extendedAPI: "http://localhost:4001",
-  explorerURL: "http://localhost:6040",
-  faucetURL: "http://localhost:3200",
+  nodes: [process.env.NODE || "http://localhost:4000"],
+  extendedAPI: process.env.EXTENDED_API || "http://localhost:4001",
+  explorerURL: process.env.EXPLORER_URL || "http://localhost:6040",
+  faucetURL: process.env.FAUCET_URL || "http://localhost:3200",
   block_time: 15000,
   sessionExpireMilliseconds: 900000,
 };
